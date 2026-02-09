9a6b80edacbd574363f9bbcfea49ba02d2e1cae3
Bartek <bartekkania98@gmail.com>
2026-02-09 15:57:46 +0100

    Lekcja 3: Wprowadzenie pętli do-while

---
+++ OpusRPG/main.cpp
@@ -0,0 +1,35 @@
+// OpusRPG.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+int main()
+{
+    std::cout << "Witaj w œwiecie Opus RPG\n";
+    std::cout << "Jak masz na imiê przyjacielu?\n";
+    std::string name;
+    std::cin >> name;
+    
+    int strength, health, agility;
+    do {
+        std::cout << "Podaj wartoœci zdrowia, si³y oraz zrêcznoœci \n Suma si³y i zrêcznoœcinie mo¿e przekraczaæ 20\n";
+        std::cin >> health >> strength >> agility;
+    } while (strength + agility > 20 || strength < 0 || agility < 0);
+    std::cout << "Statystyki zaakceptowane!\n";
+
+    if (name == "Geralt") {
+        std::cout << "Witaj, WiedŸminie!\n";
+    }
+    std::cout << "Witaj " << name << "\nsi³a: " << strength << "\nzrêcznoœæ " << agility << "\nzdrowie " << health << '\n';
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
